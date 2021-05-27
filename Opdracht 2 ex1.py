b876ea460d0494eecefff71c5f6391e91762a357
H-Haydarie <77728406+H-Haydarie@users.noreply.github.com>
2021-05-27 16:21:25 +0200

    Add files via upload

---
+++ Opdracht 2 ex1.py
@@ -0,0 +1,40 @@
+# Ex1
+# Create two variables x and y. Assign a numerical value to both variables.
+x = 5
+y = 10
+
+# Print the values of x and y.
+print(y)
+print(x)
+
+# Create a third variable named z. The value of z should be the sum of x and y.
+z = x+y
+
+#Print the value of z.
+print (z)
+
+
+
+# Ex2
+# Create a variable name. The value of name should be your name.
+name = 'Haydar'
+
+# Print the text “Hello, <your name here>!”. Use name in the print statement.
+print ('Hello, '+name+'!')
+
+
+
+# Ex3
+# Create a variable and assign a value to it.
+x = "Haydar"
+
+# Print the text “Value 1: <value 1 here>”.
+print(x)
+
+# Change the value of that same variable.
+x = "Tom"
+
+# Print the text “Value 2: <value 2 here>”.
+print(x)
+x = 6
+print(x)
